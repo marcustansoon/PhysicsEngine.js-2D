9200dcca614e5109cb1b968eb4a515abea7e61a8
marcustansoon <36304030+marcustansoon@users.noreply.github.com>
2018-07-24 20:20:57 +0800

    Update PIXI.KinematicMotion.js

--- LearnPixiJS/KinematicMotion/PIXI.KinematicMotion.js
+++ LearnPixiJS/KinematicMotion/PIXI.KinematicMotion.js
@@ -1,3 +1,4 @@
+//https://rawgit.com/marcustansoon/PhysicsEngine.js-2D/master/LearnPixiJS/KinematicMotion/PIXI.KinematicMotion.js
 //example usage : https://jsfiddle.net/6ad5ugs3/127/ 
 //example usage : https://jsfiddle.net/ftz5n6co/4/   --cleaner version
 
