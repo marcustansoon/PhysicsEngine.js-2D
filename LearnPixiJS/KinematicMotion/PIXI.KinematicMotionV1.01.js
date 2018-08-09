cb6e6eb100a93ed40e16ca7e3a69130c979a31ac
marcustansoon <36304030+marcustansoon@users.noreply.github.com>
2018-08-09 16:36:53 +0800

    Update PIXI.KinematicMotionV1.01.js

--- LearnPixiJS/KinematicMotion/PIXI.KinematicMotionV1.01.js
+++ LearnPixiJS/KinematicMotion/PIXI.KinematicMotionV1.01.js
@@ -16,110 +16,87 @@
 //attachable callback
 //	sprite.Kinematic[MOTION_NAME].onUpdate=func()   	-> triggered on each Motion.updateMotion method
 
+const KinematicConstant = 1 / 180 * Math.PI; //degree to rad formula
+class KinematicMotion {
+    constructor() {
 
-let KinematicGroup=new Set();//used to keep track of sprites with kinematic motion added and update their movement respectively
-const KinematicConstant = 1/180*Math.PI;//degree to rad formula
-class KinematicMotion
-{
-	  constructor()
-	  {
+    }
+    //able to either add new motion or edit existing motion
+    addMotion(obj, name, sprite) //obj-> angle:degree,v:velocity,a:acceleration
+    {
 
-	  }
-	  registerChild(sprite)//register a sprite, enabling kinematic motion to be added without errors
-	  {
-		sprite.Kinematic={};//store kinematic motion properties
-	   	sprite.KinematicList=new Set();//store kinematic motions' names
-		sprite.Kinematic.onUpdate=null;
-	  }
-	  unregisterChild(sprite)//unregister a sprite
-	  {
-		delete sprite.Kinematic;//remove all kinematic-added properties
-	  	sprite.KinematicList.clear();
-	  	delete sprite.KinematicList;
-	  	KinematicGroup.delete(sprite);//remove sprite from kinematic group
-	  }
-	  //able to either add new motion or edit existing motion
-	  addMotion(obj,name,sprite)//obj-> angle:degree,v:velocity,a:acceleration
-	  {
-		let temp = obj.angle*KinematicConstant,//convert to rad.
-	    	cos = Math.cos(temp),
-	   	sin = Math.sin(temp);
+        let temp = obj.angle * KinematicConstant, //convert to rad.
+            cos = Math.cos(temp),
+            sin = Math.sin(temp);
+	    
+        if (!sprite.KinematicMotions) {
+            sprite.KinematicMotions = {};
+        }
+        if (Math.abs(cos) < 0.00001) //check if value is close to zero after conversion
+        {
+            cos = 0; //if so, assigne value zero to it
+        }
+        if (Math.abs(sin) < 0.00001) //check if value is close to zero after conversion
+        {
+            sin = 0;
+        }
+        if (!this.hasMotion(name, sprite)) //check if motion alrdy attached to sprite
+        {
+            sprite.KinematicMotions[name] = {};
+        }
 
-	    if (Math.abs(cos)<0.00001)//check if value is close to zero after conversion
-	    {
-		cos=0;//if so, assigne value zero to it
-	    }
-	    if (Math.abs(sin)<0.00001)//check if value is close to zero after conversion
-	    {
-		sin=0;
-	    }
+        sprite.KinematicMotions[name].incrementX = cos * obj.v; //cal the amount of fixed increment for x
+        sprite.KinematicMotions[name].previousTruncatedX = 0; //used to store the truncated decimal potion of x
 
-	    if (!this.hasMotion(name,sprite))//check if motion alrdy attached to sprite
-	    {
-		sprite.KinematicList.add(name);//push kinematic name into sprite kinematic arr
-	     	KinematicGroup.add(sprite);	//add sprite into kinematic group
-		sprite.Kinematic[name]={};
-	      	//console.log('created '+name);
-	    }
+        sprite.KinematicMotions[name].incrementY = sin * obj.v; //cal the amount of fixed increment for y
+        sprite.KinematicMotions[name].previousTruncatedY = 0; //used to store the truncated decimal potion of y
 
-    
-	    sprite.Kinematic[name].incrementX=cos*obj.v;//cal the amount of fixed increment for x
-	    sprite.Kinematic[name].previousTruncatedX=0;//used to store the truncated decimal potion of x
+        sprite.KinematicMotions[name].aX = cos * obj.a; //store the fixed acceleration
+        sprite.KinematicMotions[name].aXMultiplier = 0; //sum up the acc for each increment of x/y
 
-	    sprite.Kinematic[name].incrementY=sin*obj.v;//cal the amount of fixed increment for y
-	    sprite.Kinematic[name].previousTruncatedY=0;//used to store the truncated decimal potion of y
+        sprite.KinematicMotions[name].aY = sin * obj.a;
+        sprite.KinematicMotions[name].aYMultiplier = 0;
 
-	    sprite.Kinematic[name].aX=cos*obj.a;//store the fixed acceleration
-	    sprite.Kinematic[name].totalAX=0;//sum up the acc for each increment of x/y
-
-	    sprite.Kinematic[name].aY=sin*obj.a;
-	    sprite.Kinematic[name].totalAY=0;
-	
-		sprite.Kinematic[name].onUpdate=null;
-	  }
-	  hasMotion(name,sprite)//check if sprite has particular motion attached
-	  {
-	    return sprite.KinematicList.has(name);
-	  }
-	  removeMotion(name,sprite)//remove motion from a sprite,given a kinematic motion's name
-	  {
-		sprite.KinematicList.delete(name);
-	    delete sprite.Kinematic[name];
-	  }
-	  updateMotion()//update movement of each kinematic's member
-	  {
-		KinematicGroup.forEach((member)=>{
-		member.KinematicList.forEach((name)=>{
-
-		let tempx=member.Kinematic[name].incrementX + member.Kinematic[name].totalAX + member.Kinematic[name].previousTruncatedX,//total up the fixed x increment, acc amount and previous truncated decimal potion
-		tempy=member.Kinematic[name].incrementY + member.Kinematic[name].totalAY + member.Kinematic[name].previousTruncatedY,
-		f_tempx = Math.floor(tempx),//round down to whole number
-		f_tempy =  Math.floor(tempy);
+        sprite.KinematicMotions[name].onUpdate = null;
+    }
+    hasMotion(name, sprite) //check if sprite has particular motion attached
+    {
+        return sprite.KinematicMotions.hasOwnProperty(name);
+    }
+    removeMotion(name, sprite) //remove motion from a sprite,given a kinematic motion's name
+    {
+        sprite.KinematicList.delete(name);
+        delete sprite.Kinematic[name];
+    }
+    updateMotion(GROUP,name) //update movement of each kinematic's member
+    {
+        //GROUP.forEach((member) => {
+	let length = GROUP.length,member;
+        for (let loop=0;loop<length;loop++){    
+		member = GROUP[loop];
+                let tempx = member.KinematicMotions[name].incrementX + member.KinematicMotions[name].aX*member.KinematicMotions[name].aXMultiplier + member.KinematicMotions[name].previousTruncatedX, //total up the fixed x increment, acc amount and previous truncated decimal potion
+                    tempy = member.KinematicMotions[name].incrementY + member.KinematicMotions[name].aY*member.KinematicMotions[name].aYMultiplier + member.KinematicMotions[name].previousTruncatedY,
+                    f_tempx = tempx<<1>>1, //round down to whole number
+                    f_tempy = tempy<<1>>1;
 
 
-		member.x += f_tempx;//for best performance, sprite x and y will be whole number, instead of decimal
-			member.Kinematic[name].previousTruncatedX=tempx-f_tempx;//keep track of the truncated decimal potion
+                member.x += f_tempx; //for best performance, sprite x and y will be whole number, instead of decimal
+                member.KinematicMotions[name].previousTruncatedX = tempx - f_tempx; //keep track of the truncated decimal potion
 
-		member.y += f_tempy;
-		member.Kinematic[name].previousTruncatedY=tempy-f_tempy;
+                member.y += f_tempy;
+                member.KinematicMotions[name].previousTruncatedY = tempy - f_tempy;
 
-		member.Kinematic[name].totalAX+=member.Kinematic[name].aX;//increase the total acc in x direction after each increment
-		member.Kinematic[name].totalAY+=member.Kinematic[name].aY;
-			
-		//invoket callback func attached to the motion name if exists
-		if (member.Kinematic[name].onUpdate){
-		    member.Kinematic[name].onUpdate();
-		    }
-	      });
-	    });
-	  }
-  
+                member.KinematicMotions[name].aXMultiplier++; //increase the total acc in x direction after each increment
+                member.KinematicMotions[name].aYMultiplier++;
+
+                //invoket callback func attached to the motion name if exists
+                if (member.KinematicMotions[name].onUpdate) {
+                    member.KinematicMotions[name].onUpdate();
+                }
+	}    
+        //});
+    }
+
 }
-if (PIXI)//check if PIXI.js exists
-{
-	PIXI.KinematicMotion=KinematicMotion;
-}
-else
-{
-	throw new Error ("PIXI undefined, please include the PIXI.js script");
-}
+
+    PIXI.KinematicMotion = KinematicMotion;
