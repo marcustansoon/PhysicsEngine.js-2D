c3af6492b6ac5c01601d98a2e0c72680a58a7a2b
marcustansoon <36304030+marcustansoon@users.noreply.github.com>
2018-09-10 12:20:05 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -12,10 +12,29 @@ function main() {
     /*
        [{ productId: 'com.yourapp.prod1', 'title': '...', description: '...', currency: '...', price: '...', priceAsDecimal: '...' }, ...]
     */
+       
+        inAppPurchase
+      .buy(productIds[0])
+      .then(function (data) {
+        alert(JSON.stringify(data));
+        /*
+          {
+            transactionId: ...
+            receipt: ...
+            signature: ...
+          }
+        */
+      })
+      .catch(function (err) {
+        alert(JSON.stringify(err));
+      });   
   })
   .catch(function (err) {
    alert('err', JSON.stringify(err) );
   });
+    
+    
+    
 }
 
 document.addEventListener('deviceready', main, false);
