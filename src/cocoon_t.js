92a1114ecafe72f34a420e844257fb8311e133b9
marcustansoon <36304030+marcustansoon@users.noreply.github.com>
2018-09-15 10:05:26 +0800

    Update cocoon_t.js

--- src/cocoon_t.js
+++ src/cocoon_t.js
@@ -1,1 +1,17 @@
 
+let renderer = new PIXI.autoDetectRenderer(250,250);
+        document.body.appendChild(renderer.view);
+        renderer.view.style.left = "0px";
+        renderer.view.style.top = "0px";
+        renderer.view.style.position = "absolute";
+	    let stage= new PIXI.Container();
+        
+        
+	let tex = new PIXI.Sprite.fromImage('https://i.imgur.com/uDuqK20.jpg');   
+    	stage.addChild(tex);
+        tex.scale.set(.5);
+        loop();
+        function loop(){
+        	renderer.render(stage);
+            requestAnimationFrame(loop);
+        }
