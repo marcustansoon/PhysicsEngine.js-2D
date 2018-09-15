604c342fc01b69a9a5d9243d3fa05cd0e30be7d8
marcustansoon <36304030+marcustansoon@users.noreply.github.com>
2018-09-15 11:58:19 +0800

    Update cocoon_t.js

--- src/cocoon_t.js
+++ src/cocoon_t.js
@@ -3,7 +3,7 @@ document.addEventListener('deviceready', initDemo, false);
 
 function initDemo(){
 	alert('rdy');
-	/*
+	
 let renderer = new PIXI.autoDetectRenderer(500,500);
         document.body.appendChild(renderer.view);
         //renderer.view.style.left = "0px";
@@ -19,5 +19,5 @@ let renderer = new PIXI.autoDetectRenderer(500,500);
         function loop(){
         	renderer.render(stage);
             requestAnimationFrame(loop);
-        }*/
+        }
 }
