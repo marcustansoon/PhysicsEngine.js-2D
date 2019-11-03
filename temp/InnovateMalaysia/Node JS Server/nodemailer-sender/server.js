7614c49c1883bfd2f4a90679d12cb81826dc9e48
marcustansoon <36304030+marcustansoon@users.noreply.github.com>
2019-11-03 20:20:22 +0800

    Create server.js

---
+++ temp/InnovateMalaysia/Node JS Server/nodemailer-sender/server.js
@@ -0,0 +1,55 @@
+let transporter = require('nodemailer').createTransport({
+    service: "Gmail",
+    auth: {
+      user: "sarawakdragonfly@gmail.com",
+      pass: "12345678Abc"
+    }
+  }),
+  app = require("http")
+    .createServer(function(req, res) {
+      if (req.url.includes("/views/activate")) {
+      }
+      console.log(req.method);
+      if (req.method == "POST") {
+        let data = "";
+        req.on("data", function(_data) {
+          data += _data;
+        });
+        req.on("end", function() {
+          //console.log(data);
+          if (IsJsonString(data)) {
+            data = JSON.parse(data);
+            if (typeof data === "object" && data.html && data.from && data.to) {
+              res.end("ok");
+              console.log("ok");
+              sendMail(data);
+            } else {
+              res.end("not ok");
+              console.log("not ok");
+            }
+          } else {
+            res.end("not ok");
+            console.log("not ok");
+          }
+        });
+      } else {
+        res.end("ok");
+      }
+    })
+    .listen(8080);
+
+function sendMail(obj) {
+  //send mail to a specified user
+  transporter.sendMail(obj, function (error, info) {
+    console.log(error);
+  });
+}
+function IsJsonString(str) {
+  //check if a string is a valid convertable object
+  try {
+    JSON.parse(str);
+  } catch (e) {
+    return false;
+  }
+  return true;
+}
