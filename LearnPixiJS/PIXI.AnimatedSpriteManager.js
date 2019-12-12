dceab7b60f27bbd0db920b472974bbb8ed77ff05
marcustansoon <36304030+marcustansoon@users.noreply.github.com>
2019-12-12 17:07:03 +0800

    Update PIXI.AnimatedSpriteManager.js

--- LearnPixiJS/PIXI.AnimatedSpriteManager.js
+++ LearnPixiJS/PIXI.AnimatedSpriteManager.js
@@ -12,17 +12,21 @@ class AnimatedSpriteManager extends PIXI.AnimatedSprite {
 //'frames' represent frames number for an animation state
   playAnimatedState(frames) {//play certain animation state 
     this.stop(); //stop the current animation
-    if (Array.isArray(frames)) //check is an animation state consists of multiple frames
+    if (frames.length) //check is an animation state consists of multiple frames
     {
-      let textures = [];//create an empty array
+      //let textures = [];//create an empty array
       if (frames.length > this._totalTextures.length) //check if total frame number exceeded total combined animation frames
       {
         throw new Error("Animated State Frames is more than total frames");
       }
-      frames.forEach(texture => { //for each 'frames' value, assign the relevant animated frames into the 'textures'
+      this.textures.length=0;
+      for(let temp=0;temp<frames.length;temp++){
+        this.textures.push(this._totalTextures[texture]);
+      }
+      /*frames.forEach(texture => { //for each 'frames' value, assign the relevant animated frames into the 'textures'
         textures.push(this._totalTextures[texture]);
-      });
-      this.textures = textures;//assign back the new animation state frames
+      });*/
+      //this.textures = textures;//assign back the new animation state frames
       this.gotoAndPlay(0); //play the animation state, start from zero
 
     } else //following statements only execute if animation state is static (consist of  a single frame)
