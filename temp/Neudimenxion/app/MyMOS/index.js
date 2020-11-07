3d228837592a2cd831a8841e79cd9e80cbe349ce
marcustansoon <36304030+marcustansoon@users.noreply.github.com>
2020-11-07 10:19:12 +0800

    Update index.js

--- temp/Neudimenxion/app/MyMOS/index.js
+++ temp/Neudimenxion/app/MyMOS/index.js
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-let redirectedURL = 'https://joinnow.my/app-view/redirection',
+let redirectedURL = 'https://mymos.my/members-account/',
     isFCMRegistered,
     FCMToken,
     FCMID,
