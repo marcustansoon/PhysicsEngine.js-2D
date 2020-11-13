48bc70f23346ea7c34a8cf283feb7395f2916fec
marcustansoon <36304030+marcustansoon@users.noreply.github.com>
2020-11-13 14:53:00 +0800

    Create index.js

---
+++ WebRTC/FileTransfer/server/index.js
@@ -0,0 +1,87 @@
+const IP = new (require("./ip"))();
+
+const server = require("http")
+  .createServer(function(req, res) {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end(IP.get(req));
+  })
+  .listen(3000);
+
+const io = require("socket.io")(server);
+const MAP = new Map();
+
+io.on("connection", client => {
+  // Upon connection, echo back the client id
+  client.emit("id", client.id);
+
+  // Get client public ip
+  client._ip = client.handshake.headers["x-forwarded-for"].split(",")[0];
+
+  // Inform other clients (of the same room) about the new client joining
+  io.to(client._ip).emit("newClient", client.id);
+
+  // Get client id
+  io.in(client._ip).clients((err, clientsInRoom) => {
+    if (err) throw new Error(err);
+    // For each existing clients in a room, sends their ids to new joining client
+    clientsInRoom.forEach(perClientInARoom =>
+      client.emit("newClient", perClientInARoom)
+    );
+  });
+
+  // Invite client into public IP room
+  client.join(client._ip);
+
+  // data -> {'key' : '', 'value' : ''}
+  client.on("setKey", function(data) {
+    MAP.set(data.key, data.value);
+  });
+
+  client.on("getValueByKey", function(key) {
+    client.emit("getValueByKey", MAP.get(key));
+  });
+
+  client.on("getId", function(key) {
+    client.emit("getId", client.id);
+  });
+
+  // data -> {'id' : '', 'message' : ''}
+  client.on("sendTo", function(data) {
+    io.to(data.id).emit("message", data.message);
+  });
+
+  // data -> {'roomName' : '', 'message' : ''}
+  client.on("sendToRoom", function(data) {
+    io.to(data.roomName).emit("message", data.message);
+  });
+
+  // Join by room name
+  client.on("joinRoom", function(roomName) {
+    client.join(roomName);
+  });
+
+  // Leave by room name
+  client.on("leaveRoom", function(roomName) {
+    client.leave(roomName);
+  });
+
+  // data -> {'from' : '...', 'to' : '...', 'offer': '...'}
+  client.on("sendOffer", function(data) {
+    io.to(data.to).emit("offer", data);
+  });
+
+  // data -> {'from' : '...', 'to' : '...', 'answer': '...'}
+  client.on("sendAnswer", function(data) {
+    io.to(data.to).emit("answer", data);
+  });
+
+  // data -> {'from' : '...', 'to' : '...', 'iceCandidate': '...'}
+  client.on("sendICE", function(data) {
+    io.to(data.to).emit("ice", data);
+  });
+
+  // data -> {'from' : '...', 'to' : '...', 'something': '...'}
+  client.on("message", function(data) {
+    io.to(data.to).emit("message", data);
+  });
+});
