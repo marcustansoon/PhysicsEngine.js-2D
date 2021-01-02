231e109a8278106d6703acc816ef5bb77fa41bfb
marcustansoon <36304030+marcustansoon@users.noreply.github.com>
2021-01-02 22:45:16 +0800

    Update index.js

--- temp/Neudimenxion/app/Reddshop/index.js
+++ temp/Neudimenxion/app/Reddshop/index.js
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-let redirectedURL = 'https://reddshop.com',
+let redirectedURL = 'https://joinnow.my/app-view/redirection',
     isFCMRegistered,
     FCMToken,
     FCMID,
