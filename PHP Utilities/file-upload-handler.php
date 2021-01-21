c780dabb3ace46bb3c17be6f1afb86d8a1002f94
marcustansoon <36304030+marcustansoon@users.noreply.github.com>
2021-01-21 22:54:23 +0800

    Update file-upload-handler.php

--- PHP Utilities/file-upload-handler.php
+++ PHP Utilities/file-upload-handler.php
@@ -1,29 +1,35 @@
 <?php 
 
 	function validateAndUploadFile(?array $file, string $uploadPath = "/", array $extensions, int $maxFileSize, bool $shouldRenameFile = true, bool $shouldOverrideFile = true){
-		// Check file existence
+		// Make sure file to be uploaded exists
 		if(!is_array($file)){
 			echo 'nope';
 			return (object)['status' => 0, 'responseMessage' => 'File to be uploaded doesn\'t exists'];
 		}
-		// Check file extension
+		
+		// Validate file extension
 		if(!in_array($fileExtension = strtolower(pathinfo($file['name'], PATHINFO_EXTENSION)), $extensions)){
 			return (object)['status' => 0, 'responseMessage' => 'File extension is not supported'];
 		}
-		// Check file size
+		
+		// Validate file size
 		if($file['size'] > $maxFileSize){
 			return (object)['status' => 0, 'responseMessage' => 'File size is too large'];
 		}
-		// Generate a random file name if required
+		
+		// Generate random file name if required
 		$fileName = $shouldRenameFile ? date('Y-m-d H.i.s A - ').dechex(rand(100,999)) : $file['name'];
 
-		// 
+		// Check if file already exists
 		if(!$shouldOverrideFile && file_exists($uploadPath.$fileName.'.'.$fileExtension)){
 			return (object)['status' => 0, 'responseMessage' => 'File already exists'];
 		}
-
+		
+		// Upload file
 		move_uploaded_file($file['tmp_name'], $uploadPath.$fileName.'.'.$fileExtension);
 
 		return (object)['status' => 1, 'responseMessage' => 'File uploaded successfully'];
 	}
+
+	// Example usage
 	echo validateAndUploadFile($_FILES['fileToUpload'] ?? null, 'uploads/', ['jpg', 'jpeg', 'png'], 1024 * 1024 *  1, false, false)->responseMessage;
