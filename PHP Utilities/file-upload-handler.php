f295d7004b1d3bcf7f0b940730f0bc6d67b50d3b
marcustansoon <36304030+marcustansoon@users.noreply.github.com>
2021-01-21 22:47:39 +0800

    Update file-upload-handler.php

--- PHP Utilities/file-upload-handler.php
+++ PHP Utilities/file-upload-handler.php
@@ -1,27 +1,29 @@
 <?php 
 
-	function validateAndUploadFile(?array $file, string $uploadPath = "/", array $extensions, int $maxFileSize, bool $shouldOverrideFile = true, bool $shouldRenameFile = true){
+	function validateAndUploadFile(?array $file, string $uploadPath = "/", array $extensions, int $maxFileSize, bool $shouldRenameFile = true, bool $shouldOverrideFile = true){
+		// Check file existence
 		if(!is_array($file)){
 			echo 'nope';
 			return (object)['status' => 0, 'responseMessage' => 'File to be uploaded doesn\'t exists'];
 		}
-
+		// Check file extension
 		if(!in_array($fileExtension = strtolower(pathinfo($file['name'], PATHINFO_EXTENSION)), $extensions)){
 			return (object)['status' => 0, 'responseMessage' => 'File extension is not supported'];
 		}
-
+		// Check file size
 		if($file['size'] > $maxFileSize){
 			return (object)['status' => 0, 'responseMessage' => 'File size is too large'];
 		}
+		// Generate a random file name if required
+		$fileName = $shouldRenameFile ? date('Y-m-d H.i.s A - ').dechex(rand(100,999)) : $file['name'];
 
-		if(!$shouldRenameFile && !$override && file_exists($uploadPath.$file['name'])){
+		// 
+		if(!$shouldOverrideFile && file_exists($uploadPath.$fileName.'.'.$fileExtension)){
 			return (object)['status' => 0, 'responseMessage' => 'File already exists'];
 		}
 
-		if(){
+		move_uploaded_file($file['tmp_name'], $uploadPath.$fileName.'.'.$fileExtension);
 
-		}
-		echo $file['name'];
-		echo dechex(time().rand(100,999));
-		return (object)['status' => 1, 'responseMessage' => 'File is uploaded successfully'];
+		return (object)['status' => 1, 'responseMessage' => 'File uploaded successfully'];
 	}
+	echo validateAndUploadFile($_FILES['fileToUpload'] ?? null, 'uploads/', ['jpg', 'jpeg', 'png'], 1024 * 1024 *  1, false, false)->responseMessage;
