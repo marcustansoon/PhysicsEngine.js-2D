31b19307b899be5fa0a7519a7adbd6a4c0d9fc44
marcustansoon <36304030+marcustansoon@users.noreply.github.com>
2021-09-12 14:35:26 +0800

    Update automated-script.js

--- temp/Requestly-IO/automated-script.js
+++ temp/Requestly-IO/automated-script.js
@@ -1,4 +1,4 @@
-// Similar functionality as Octokit.JS (https://octokit.github.io/rest.js/v18/)
+	// Similar functionality as Octokit.JS (https://octokit.github.io/rest.js/v18/)
 	function mfetch (url, replacer, header) {
 		let GETparams = {}
 		for (const property in replacer) {
@@ -11,28 +11,46 @@
 		url = url + '?' + (new URLSearchParams(GETparams)).toString()
 		return fetch(url, header)
 	}
-
-	async function getModifiedSourceCode () {
-		const MODIFIED_CONTENT_TO_BE_REPLACED = `'[{"id":"Users","title":"Number of Users","bronze":{"title":"Single User","enabled":true},"silver":{"title":"Unlimited Users","enabled":false},"gold":{"title":"Single User","enabled":true},"enterprise":{"title":"Minimum 20 Users","enabled":true}},{"id":"rules","title":"Rules","bronze":{"title":"10 Rules","limit":50,"enabled":true},"silver":{"title":"25 Rules (Max 10 Active)","limit":25,"enabled":true},"gold":{"title":"Unlimited Rules","limit":10000,"enabled":true},"enterprise":{"title":"Unlimited Rules","limit":10000,"enabled":true}},{"id":"active_rules","title":"Active Rules","bronze":{"title":"3 Active Rules","limit":50,"enabled":true},"silver":{"title":"10 Active Rules","limit":50,"enabled":true},"gold":{"title":"Unlimited Active Rules","limit":10000,"enabled":true},"enterprise":{"title":"Unlimited Active Rules","limit":10000,"enabled":true}},{"id":"favourite_rules","title":"Favourite Rules","bronze":{"title":"3 Favourite Rules","limit":3,"enabled":true},"silver":{"title":"10 Favourite Rules","limit":10,"enabled":true},"gold":{"title":"Unlimited Favourite Rules","limit":10000,"enabled":true},"enterprise":{"title":"Unlimited Favourite Rules","limit":10000,"enabled":true}},{"id":"files","title":"Files","bronze":{"title":"5 Files","limit":50,"enabled":true},"silver":{"title":"10 Files","limit":10,"enabled":true},"gold":{"title":"Unlimited Files","limit":1000,"enabled":true},"enterprise":{"title":"Unlimited Files","limit":1000,"enabled":true}},{"id":"groups","title":"Groups","bronze":{"title":"Unlimited Groups","enabled":true},"silver":{"title":"Unlimited Groups","enabled":true},"gold":{"title":"Unlimited Groups","enabled":true},"enterprise":{"title":"Unlimited Groups","enabled":true}},{"id":"shared_lists","title":"Shared Lists","bronze":{"title":"10 Shared Lists","limit":50,"enabled":true},"silver":{"title":"10 Shared Lists","limit":10,"enabled":true},"gold":{"title":"Unlimited Shared Lists","limit":1000,"enabled":true},"enterprise":{"title":"Unlimited Shared Lists","limit":1000,"enabled":true}},{"id":"rule_pairs","title":"Rule Pairs","bronze":{"title":"3 Rule Pairs","limit":50,"enabled":true},"silver":{"title":"10 Rule Pairs","limit":10,"enabled":true},"gold":{"title":"Unlimited Rule Pairs","limit":10000,"enabled":true},"enterprise":{"title":"Unlimited Rule Pairs","limit":10000,"enabled":true}},{"id":"RESPONSE_BODY_CHARACTER_LIMIT","title":"AJAX Response Char Limit","showInComparisonTable":false,"bronze":{"title":"Upto 6,000 Characters AJAX Response","limit":6000,"enabled":true},"silver":{"title":"Upto 6,000 Characters AJAX Response","limit":6000,"enabled":true},"gold":{"title":"Upto 6,000 Characters AJAX Response","limit":6000,"enabled":true},"enterprise":{"title":"Upto 6,000 Characters AJAX Response","limit":6000,"enabled":true}},{"id":"export_import","title":"Export & Import Rules","bronze":{"title":"Export Rules","enabled":true},"silver":{"title":"Import & Export Rules","enabled":true},"gold":{"title":"Import & Export Rules","enabled":true},"enterprise":{"title":"Import & Export Rules","enabled":true}},{"id":"Customer Support","title":"Priority Support","bronze":{"title":"Support","enabled":false},"silver":{"title":"Priority Support","enabled":false},"gold":{"title":"Priority Support (Email & Chat)","enabled":true},"enterprise":{"title":"Priority Support (Chat & Call)","enabled":true}}]'`,
-		originalContentToBeReplaced_2 = `CUSTOM_SCRIPT_CHARACTER_LIMIT:500`,
-		MODIFIED_CONTENT_TO_BE_REPLACED_2 = `CUSTOM_SCRIPT_CHARACTER_LIMIT:2500`
-		// Get script elem
-		let elem = [...document.querySelectorAll('script')].find(elem => elem.src.includes('/static/js/main.')),
-		// Get script raw source code
-		sourceCode = await fetch(elem.src).then(response => response.text()),
-		// Search for index
-		firstIndex = sourceCode.indexOf(`'[{"id":"Users","title":"Number of Users"`),
-		secondIndex = sourceCode.indexOf(`')},,,,,,,,,,,`, firstIndex) + 1,
-		originalContentToBeReplaced = sourceCode.substring(firstIndex, secondIndex),
-		// Replacement of original source code
-		modifiedSourceCode = sourceCode.replace(originalContentToBeReplaced, MODIFIED_CONTENT_TO_BE_REPLACED)
-		// Replacement 2
-		modifiedSourceCode = modifiedSourceCode.replace(originalContentToBeReplaced_2, MODIFIED_CONTENT_TO_BE_REPLACED_2)
-		return modifiedSourceCode
+	
+	String.prototype.replaceArray = function(find, replace) {
+		var replaceString = this;
+		for (var i = 0; i < find.length; i++) {
+    			replaceString = replaceString.replace(find[i], replace[i]);
+  		}
+  		return replaceString;
 	}
 
+	// Return modified source code
+	async function getModifiedSourceCode () {
+		const searchTerms = [], replacementTerms = []
+		
+		// Get script elem
+		let elem = [...document.querySelectorAll('script')].find(elem => elem.src.includes('/static/js/main.')),
+		    
+		// Get script raw source code
+		sourceCode = await fetch(elem.src).then(response => response.text()),
+		    
+		// Search for index
+		firstIndex = sourceCode.indexOf(`'[{"id":"Users","title":"Number of Users"`),
+		secondIndex = sourceCode.indexOf(`')},,,,,,,,,,,`, firstIndex) + 1
+		
+		// Define search terms
+		searchTerms.push(sourceCode.substring(firstIndex, secondIndex))
+		searchTerms.push(`CUSTOM_SCRIPT_CHARACTER_LIMIT:500`)
+		
+		// Define replacement terms
+		replacementTerms.push(`'[{"id":"Users","title":"Number of Users","bronze":{"title":"Single User","enabled":true},"silver":{"title":"Unlimited Users","enabled":false},"gold":{"title":"Single User","enabled":true},"enterprise":{"title":"Minimum 20 Users","enabled":true}},{"id":"rules","title":"Rules","bronze":{"title":"10 Rules","limit":50,"enabled":true},"silver":{"title":"25 Rules (Max 10 Active)","limit":25,"enabled":true},"gold":{"title":"Unlimited Rules","limit":10000,"enabled":true},"enterprise":{"title":"Unlimited Rules","limit":10000,"enabled":true}},{"id":"active_rules","title":"Active Rules","bronze":{"title":"3 Active Rules","limit":50,"enabled":true},"silver":{"title":"10 Active Rules","limit":50,"enabled":true},"gold":{"title":"Unlimited Active Rules","limit":10000,"enabled":true},"enterprise":{"title":"Unlimited Active Rules","limit":10000,"enabled":true}},{"id":"favourite_rules","title":"Favourite Rules","bronze":{"title":"3 Favourite Rules","limit":3,"enabled":true},"silver":{"title":"10 Favourite Rules","limit":10,"enabled":true},"gold":{"title":"Unlimited Favourite Rules","limit":10000,"enabled":true},"enterprise":{"title":"Unlimited Favourite Rules","limit":10000,"enabled":true}},{"id":"files","title":"Files","bronze":{"title":"5 Files","limit":50,"enabled":true},"silver":{"title":"10 Files","limit":10,"enabled":true},"gold":{"title":"Unlimited Files","limit":1000,"enabled":true},"enterprise":{"title":"Unlimited Files","limit":1000,"enabled":true}},{"id":"groups","title":"Groups","bronze":{"title":"Unlimited Groups","enabled":true},"silver":{"title":"Unlimited Groups","enabled":true},"gold":{"title":"Unlimited Groups","enabled":true},"enterprise":{"title":"Unlimited Groups","enabled":true}},{"id":"shared_lists","title":"Shared Lists","bronze":{"title":"10 Shared Lists","limit":50,"enabled":true},"silver":{"title":"10 Shared Lists","limit":10,"enabled":true},"gold":{"title":"Unlimited Shared Lists","limit":1000,"enabled":true},"enterprise":{"title":"Unlimited Shared Lists","limit":1000,"enabled":true}},{"id":"rule_pairs","title":"Rule Pairs","bronze":{"title":"3 Rule Pairs","limit":50,"enabled":true},"silver":{"title":"10 Rule Pairs","limit":10,"enabled":true},"gold":{"title":"Unlimited Rule Pairs","limit":10000,"enabled":true},"enterprise":{"title":"Unlimited Rule Pairs","limit":10000,"enabled":true}},{"id":"RESPONSE_BODY_CHARACTER_LIMIT","title":"AJAX Response Char Limit","showInComparisonTable":false,"bronze":{"title":"Upto 6,000 Characters AJAX Response","limit":6000,"enabled":true},"silver":{"title":"Upto 6,000 Characters AJAX Response","limit":6000,"enabled":true},"gold":{"title":"Upto 6,000 Characters AJAX Response","limit":6000,"enabled":true},"enterprise":{"title":"Upto 6,000 Characters AJAX Response","limit":6000,"enabled":true}},{"id":"export_import","title":"Export & Import Rules","bronze":{"title":"Export Rules","enabled":true},"silver":{"title":"Import & Export Rules","enabled":true},"gold":{"title":"Import & Export Rules","enabled":true},"enterprise":{"title":"Import & Export Rules","enabled":true}},{"id":"Customer Support","title":"Priority Support","bronze":{"title":"Support","enabled":false},"silver":{"title":"Priority Support","enabled":false},"gold":{"title":"Priority Support (Email & Chat)","enabled":true},"enterprise":{"title":"Priority Support (Chat & Call)","enabled":true}}]'`)
+		replacementTerms.push(`CUSTOM_SCRIPT_CHARACTER_LIMIT:2500`)
+		    
+		// Replacement of original source code
+		modifiedSourceCode = sourceCode.replaceArray(searchTerms, replacementTerms)
+		
+		return modifiedSourceCode
+	}
+	
+	// Push new file to repo
 	async function mRun (_token) {
-		if(!localStorage.getItem("token")) localStorage.setItem("token", _token || prompt("Please enter your token", ""));
+		if(!localStorage.getItem("token")) localStorage.setItem("token", _token || prompt("Please enter your token", ""))
 		const token = localStorage.getItem("token")
 		let modifiedSourceCode = await getModifiedSourceCode(),
 		randomHexValue = Math.floor(Math.random()*16777215).toString(16),
@@ -46,7 +64,7 @@
 			    'method': 'PUT',
 			    'body': JSON.stringify({
 				    'message': `Create ${fileName} - automated`,
-				    'content': btoa(modifiedSourceCode)
+				    'content': btoa(unescape(encodeURIComponent(modifiedSourceCode)))
 	    		}),
 		  		'headers': {
 		    		'Authorization': `token ${token}`
@@ -55,5 +73,5 @@
 		)
 		.then(res => res.json())
 		.then(json => console.log(json))
-		console.log(fileName)
+		console.log('Creating ' + fileName + ' ...')
 	}
