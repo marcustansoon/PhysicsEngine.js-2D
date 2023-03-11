cb2d5969e41eba0f43ba58c2aa18d19c5c2c9517
marcustansoon <36304030+marcustansoon@users.noreply.github.com>
2023-03-12 02:28:18 +0800

    Create script.js

---
+++ SB/script.js
@@ -0,0 +1,31 @@
+const observer = new IntersectionObserver(entries => {
+  //console.log(entries)
+  entries.forEach(entry => {
+    const square = entry.target.parentNode.querySelector('.image');
+
+    if (entry.isIntersecting) {
+      square.classList.add('slide-animation');
+      return; // if we added the class, exit the function
+    }
+
+    // We're not intersecting, so remove the class!
+    square.classList.remove('slide-animation');
+  });
+});
+
+document.querySelectorAll('.body').forEach(elem => observer.observe(elem));
+
+function openFullscreen() {
+  let elem = document.body;
+	if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) {
+    /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+confirm('Go fullscreen ?') && openFullscreen();
