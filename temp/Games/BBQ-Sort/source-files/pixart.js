11ce55b304f4aebe3fee8d2966b3a476fa6842a3
marcustansoon <36304030+marcustansoon@users.noreply.github.com>
2024-06-27 16:07:07 +0800

    Create pixart.js

---
+++ temp/Games/BBQ-Sort/source-files/pixart.js
@@ -0,0 +1,32 @@
+xexobo3596@kinsef.com
+	
+(async () => {
+  const rawResponse = await fetch('https://ai.picsart.com/gw1/text-to-image-middleware/ai-expand/v1/', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    'Userid':457160171027101,
+'Username':'xexobo3596'
+    },
+    body: JSON.stringify({ "batch_size_web": 2, "batch_size_mobile": 2, "expand_service_url": "/text-to-image-middleware/ai-expand/v1/", "task_type": "dreamshaper", "negative_prompt": "frame, circle frame, collage, grid, image in the collage, square frame, square boxes, divided squares, picture, split, split canvas, canvas, before after image, framed image, canvas in the form of frame, picture in the form of frame, image in the frame, framed picture, canvas in the frame, blur, text, watermark, written words, letters, extra feet, extra hands, extra people", "positive_prompt": "continue the image, continue the view, keep the colors, food on round table, top down view on table, no human, no people", "sampling_method": "PNDM", "inpaint_area": "whole_picture", "request_type": "async", "paint_mask": false, "sampling_steps": 50, "size": "1024x1024", "blend": true, "guidance_scale": 7.5, "mask_blur": 0, "inpaint_mask_padding": 32, "masked_content": "latent_noise", "caption": "", "padding": 
+[250, 250, 250, 250], "batch_size": 1, "image_url": "https://cdnmf.picsart.com/cloud-storage/92e1a70e-1504-45ab-add9-3aa9b8bdd535.jpeg" })
+  });
+  const content = await rawResponse.json();
+
+  console.log(content);
+})();
+
+
+
+
+
+fetch('https://ai.picsart.com/gw1/text-to-image-middleware/ai-expand/v1/86e0638a-7ab8-4fe5-80d9-d874a995da42', {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+        'Userid':457160171027101,
+        'Username':'xexobo3596',
+    }}
+    ).then(e=>e.json()).then(console.log)
