7b56c6b5ba088d82ca4e88f84f1c4353a6f49e8d
marcustansoon <36304030+marcustansoon@users.noreply.github.com>
2024-06-28 01:00:44 +0800

    Update index.js

--- temp/App Development/BBQ-Sort-Game/index.js
+++ temp/App Development/BBQ-Sort-Game/index.js
@@ -1,7 +1,7 @@
 let ref;
 document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
-    alert(/*cordova.file*/1);
+    alert(/*cordova.file*/'1');
     setTimeout(()=>{
     ref = cordova.InAppBrowser.open("https://www.joinnow.my/", '_blank', 'location=no,hideurlbar=yes,toolbar=no,zoom=no');
     }, 2000);
