a04d5b181738d877dee37f4f6fbb868b91bbe0b1
marcustansoon <36304030+marcustansoon@users.noreply.github.com>
2024-07-17 16:24:15 +0800

    Update script.js

--- temp/App Development/BBQ-Sort-Game/script.js
+++ temp/App Development/BBQ-Sort-Game/script.js
@@ -78,7 +78,6 @@ let ref,
 					gameLevelToBeUpdated = e.data.data.gameLevel
 					this.startUpdateGameLevelInterval();
 				} else if (e.data.type === 'get-user-data') {
-					alert('a')
 					this.IABReply({ 
 						"type": "get-user-data",
 						"data": userData,
