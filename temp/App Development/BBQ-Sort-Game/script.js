e11226d2bd89a9e4620bde3f631c873a1ab5d098
marcustansoon <36304030+marcustansoon@users.noreply.github.com>
2024-07-17 15:25:13 +0800

    Update script.js

--- temp/App Development/BBQ-Sort-Game/script.js
+++ temp/App Development/BBQ-Sort-Game/script.js
@@ -60,7 +60,7 @@ let ref,
 				this.requestUserData();
 			}, 15000);
 			setTimeout(() => {
-				ref = cordova.InAppBrowser.open("https://marcustansoon.github.io/PhysicsEngine.js-2D/temp/App%20Development/BBQ-Sort-Game/index.html", '_blank', 'location=no,hideurlbar=yes,toolbar=no,zoom=no');
+				ref = cordova.InAppBrowser.open("https://marcustansoon.github.io/PhysicsEngine.js-2D/temp/Games/BBQ-Sort/source-files/combined-index.html", '_blank', 'location=no,hideurlbar=yes,toolbar=no,zoom=no');
 				this.addIABEventListener();
 			}, 500);
 		},
