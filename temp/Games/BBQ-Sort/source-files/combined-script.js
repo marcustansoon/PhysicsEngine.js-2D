f986f11fb171a9e7e6cb25ee974954af3e225801
marcustansoon <36304030+marcustansoon@users.noreply.github.com>
2024-07-18 13:38:50 +0800

    Update combined-script.js

--- temp/Games/BBQ-Sort/source-files/combined-script.js
+++ temp/Games/BBQ-Sort/source-files/combined-script.js
@@ -2804,8 +2804,8 @@
         case "ZOOM-IN-ACC":
           this.banner.scale.x = this.scaleBanner * 4 + this.count;
           this.banner.scale.y = this.scaleBanner * 4 + this.count;
-          this.count -= (this.scaleBanner * 4) / 130;
-          this.alpha += 0.001;
+          this.count -= (this.scaleBanner * 4) / 100;
+          this.alpha += 0.006;
           this.light1.alpha = Math.min(1, this.alpha);
           this.light2.alpha = Math.min(1, this.alpha);
           this.banner.alpha = Math.min(1, this.alpha);
@@ -2817,7 +2817,7 @@
         case "ZOOM-IN":
           this.banner.scale.x = this.scaleBanner * 4 + this.count;
           this.banner.scale.y = this.scaleBanner * 4 + this.count;
-          this.count -= ((this.scaleBanner * 4) / 130) * 1.2;
+          this.count -= ((this.scaleBanner * 4) / 100) * 1.2;
           this.alpha += 0.01;
           this.light1.alpha = Math.min(1, this.alpha);
           this.light2.alpha = Math.min(1, this.alpha);
